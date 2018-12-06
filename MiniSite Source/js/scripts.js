c407c052a2521783eb60e01b3a29cdfa06272c1a
tommaddoxsoftware <tommaddox1997uk@gmail.com>
2018-12-06 06:04:47 +0000

    Completed minisite structure, added content and changed design slightly

--- MiniSite Source/js/scripts.js
+++ MiniSite Source/js/scripts.js
@@ -1,13 +1,29 @@
 
-jQuery(document).ready(function() {
-	$('a[href*="#"]').on('click', function(e) {
-		e.preventDefault();
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-		$("html,body").animate({ scrollTo: $($(this).attr("href"))}, "slow");
+      // Store hash
+      var hash = this.hash;
 
-		console.log("Height: " + $($(this).attr("href")).offset().top);
-	});
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+
+	  console.log($(hash).offset().top);
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 
 	// toggle "navbar-no-bg" class
 	$('.intro .text').waypoint(function() {
