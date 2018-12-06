61ceb90794e49a8ced23a80fcda3ae47047dfd46
tommaddoxsoftware <tommaddox1997uk@gmail.com>
2018-12-06 00:39:14 +0000

    Added basic collision detection script (NOT FINISHED, REQUIRES MERGE)

---
+++ Unity Project/VirtualFeel Haptics System/Assets/Scripts/Player/CollisionDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour {
+
+    [SerializeField]
+    private string HitboxName;
+    [SerializeField]
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Wall")
+        {
+
+        }
+    }
+
+}
